dd4b7ee2c109287e8cda7667acf418000e4c0d0d
calfgz <zhong.calf@gmail.com>
2020-03-12 18:05:50 +0800

    config bus

--- config-client/src/main/java/cn/calfgz/springcloud/controller/TestController.java
+++ config-client/src/main/java/cn/calfgz/springcloud/controller/TestController.java
@@ -1,6 +1,7 @@
 package cn.calfgz.springcloud.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2020-03-12 16:43
  */
 @RestController
+@RefreshScope
 public class TestController {
 
     @Value("${app.info.name}")
