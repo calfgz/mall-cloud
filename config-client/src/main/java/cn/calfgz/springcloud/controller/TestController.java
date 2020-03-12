e64926a5e54ca604467630e9d96619e406cb57b3
calfgz <zhong.calf@gmail.com>
2020-03-12 16:46:23 +0800

    config client

---
+++ config-client/src/main/java/cn/calfgz/springcloud/controller/TestController.java
@@ -0,0 +1,22 @@
+package cn.calfgz.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author calfgz
+ * @description:
+ * @date 2020-03-12 16:43
+ */
+@RestController
+public class TestController {
+
+    @Value("${app.info.name}")
+    private String appInfoName;
+
+    @GetMapping("/info")
+    public String info() {
+        return appInfoName;
+    }
+}
