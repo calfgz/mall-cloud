cab27ed3541f4d45c08ecba6d3dd1888747637da
calfgz <zhong.calf@gmail.com>
2020-03-15 21:09:33 +0800

    spring cloud alibaba nacos

--- payment-nacos/src/main/java/cn/calfgz/springcloud/controller/PaymentController.java
+++ payment-nacos/src/main/java/cn/calfgz/springcloud/controller/PaymentController.java
@@ -1,7 +1,7 @@
 package cn.calfgz.springcloud.controller;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,14 +12,17 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2020-03-13 18:03
  */
 @RestController
-@Slf4j
+@RefreshScope
 public class PaymentController {
+
+    @Value("${config.info}")
+    private String configInfo;
 
     @Value("${server.port}")
     private String serverPort;
 
     @GetMapping("/payment/nacos/{id}")
     public String get(@PathVariable("id") String id) {
-        return "nacos registry. serverPort:" + serverPort + ", id:" + id;
+        return "nacos registry. serverPort:" + serverPort + ", configInfo:" + configInfo + ", id:" + id;
     }
 }
